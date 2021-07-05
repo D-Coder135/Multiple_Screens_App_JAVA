b3eaccfb3f1730be5bf380532ecc2df69cc09f2a
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-05 23:12:57 +0530

    initialize the nextPageButton object

--- app/src/main/java/com/example/multiple_screens_appjava/SecondPage.java
+++ app/src/main/java/com/example/multiple_screens_appjava/SecondPage.java
@@ -12,5 +12,6 @@ public class SecondPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_page);
+        nextPageButton = findViewById(R.id.button2);
     }
 }
