f15ff74d604723f11d919b00df5dff09dd4a6a69
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-05 22:47:22 +0530

    add one more empty activity ThirdPage to the Project

---
+++ app/src/main/java/com/example/multiple_screens_appjava/ThirdPage.java
@@ -0,0 +1,14 @@
+package com.example.multiple_screens_appjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ThirdPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third_page);
+    }
+}
