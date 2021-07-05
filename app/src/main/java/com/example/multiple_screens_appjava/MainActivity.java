cc2faa4b9d716075cb4e8725bbab0a866ac1f4fe
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-05 23:04:46 +0530

    define the object for the Button class

--- app/src/main/java/com/example/multiple_screens_appjava/MainActivity.java
+++ app/src/main/java/com/example/multiple_screens_appjava/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.multiple_screens_appjava;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button nextPageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
