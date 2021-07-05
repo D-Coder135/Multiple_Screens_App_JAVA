88f2fc66018c1a9405d760a1ecadc3822e924374
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-05 23:41:30 +0530

    initialize the object backButton

--- app/src/main/java/com/example/multiple_screens_appjava/ThirdPage.java
+++ app/src/main/java/com/example/multiple_screens_appjava/ThirdPage.java
@@ -12,5 +12,6 @@ public class ThirdPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third_page);
+        backButton = findViewById(R.id.button4);
     }
 }
