33af00da4c58a030f979992fd5129a3a8286dfe0
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-05 23:42:09 +0530

    call the setOnClickListener() method along with the backButton object

--- app/src/main/java/com/example/multiple_screens_appjava/ThirdPage.java
+++ app/src/main/java/com/example/multiple_screens_appjava/ThirdPage.java
@@ -2,7 +2,9 @@ package com.example.multiple_screens_appjava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class ThirdPage extends AppCompatActivity {
@@ -13,5 +15,13 @@ public class ThirdPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third_page);
         backButton = findViewById(R.id.button4);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ThirdPage.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
