abb14f996513d8940bc0a16cac1feaf2061767c8
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-05 23:07:45 +0530

    call the setOnClickListener() method along with the object nextPageButton and add the object of the Intent class inside it

--- app/src/main/java/com/example/multiple_screens_appjava/MainActivity.java
+++ app/src/main/java/com/example/multiple_screens_appjava/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +16,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         nextPageButton = findViewById(R.id.button);
 
-        Intent intent = new Intent(MainActivity.this, SecondPage.class);
+        nextPageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondPage.class);
+            }
+        });
     }
 }
