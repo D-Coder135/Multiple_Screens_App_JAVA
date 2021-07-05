d258488fa4b608470d1353b932baacf36b53eb9d
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-05 23:12:18 +0530

    define the objects for the Button class

--- app/src/main/java/com/example/multiple_screens_appjava/SecondPage.java
+++ app/src/main/java/com/example/multiple_screens_appjava/SecondPage.java
@@ -3,8 +3,10 @@ package com.example.multiple_screens_appjava;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class SecondPage extends AppCompatActivity {
+    Button nextPageButton, backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
