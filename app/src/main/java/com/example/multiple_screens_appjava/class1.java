9b6e4e9b2864eec367fe5cb03cebbb9b46c374bc
Abdus Shafi <abdusshsfi99@gmail.com>
2021-11-18 01:32:56 +0600

    master branch commit

---
+++ app/src/main/java/com/example/multiple_screens_appjava/class1.java
@@ -0,0 +1,24 @@
+package com.example.multiple_screens_appjava;
+
+public class class1 {
+
+    int a,b;
+    public class1() {
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+}
